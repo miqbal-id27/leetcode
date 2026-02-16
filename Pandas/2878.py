b36c139515f85e1c3d6793f888280400e4fb0f85
miqbal.id27 <miqbal.id27@gmail.com>
2026-02-17 03:13:19 +1100

    Intro to Pandas - 17 Feb 2026

---
+++ Pandas/2878.py
@@ -0,0 +1,35 @@
+## 2878. Get the Size of a DataFrame
+# https://leetcode.com/problems/get-the-size-of-a-dataframe/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+
+"""
+DataFrame players:
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| player_id   | int    |
+| name        | object |
+| age         | int    |
+| position    | object |
+| ...         | ...    |
++-------------+--------+
+Write a solution to calculate and display the number of rows and columns of players.
+
+Return the result as an array:
+
+[number of rows, number of columns]
+
+The result format is in the following example.
+"""
+
+"""
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return [len(players), len(players.columns)]
+"""
+
+## Optimized solutionos from others
+"""
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
+"""
